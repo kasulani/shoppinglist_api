dc39fce344abc5b6f25466a744d4d6b9a771f279
Emmanuel King Kasulani <kasulani@gmail.com>
2017-10-16 14:44:21 +0300

    refactors code to meet pep8 standards

--- app/utility.py
+++ app/utility.py
@@ -15,6 +15,7 @@ def get_token():
         shoplist_api.logger.error(ex.message)
         return None
 
+
 def select_func_to_return(f, *args, **kwargs):
     """
     This function selects which function to return depending on number of parameters the decorated function expects
@@ -27,6 +28,7 @@ def select_func_to_return(f, *args, **kwargs):
     except Exception as ex:
         shoplist_api.logger.error(ex.message)
         return make_response(jsonify({'status': 'fail', 'message': ex.message})), 500
+
 
 def validate_content_type(f):
     """This function validates the content type in the request"""
